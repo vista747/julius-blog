0d9f9010ab77762dc4812736d52519c764cd23f9
vista747 <julius@julius.cool>
2026-02-08 12:36:49 -0500

    Add Google Analytics gtag and remove hibp file
    Inject Google Analytics gtag into page heads: added snippet to index.html, discord.html, and richard.html, and updated head.js to prepend the gtag when loading /rss-head.html. Removed hibp-verification.txt and its entry from .gitignore. Small CSS cleanups: whitespace/comment tweaks and removal of obsolete phone-dropdown rules and minor selector formatting adjustments.

--- head.js
+++ head.js
@@ -1,5 +1,16 @@
 fetch("/rss-head.html")
   .then(res => res.text())
   .then(html => {
-    document.head.insertAdjacentHTML("beforeend", html);
+    const gtag = `<!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-51Z344C42L"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);} 
+    gtag('js', new Date());
+
+    gtag('config', 'G-51Z344C42L');
+  </script>`;
+
+    // insert gtag first, then rss head
+    document.head.insertAdjacentHTML("beforeend", gtag + html);
   });
